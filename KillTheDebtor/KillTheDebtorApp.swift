ce7cc2cadba5e48537841c0abaa1b311ec68f1dc
Pavel <malinatrashh@gmail.com>
2023-04-07 18:14:02 +0800

    Initial Commit

---
+++ KillTheDebtor/KillTheDebtorApp.swift
@@ -0,0 +1,17 @@
+//
+//  KillTheDebtorApp.swift
+//  KillTheDebtor
+//
+//  Created by Pavel Naumov on 07.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct KillTheDebtorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
